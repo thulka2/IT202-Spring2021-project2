a8a17a6c1cdeef19904f4d11ed402b9a02f2cfb8
thulka2 <thulka2@uic.edu>
2021-03-27 22:47:28 -0500

    fixed path

--- script.js
+++ script.js
@@ -250,7 +250,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then((registration) => {
+      navigator.serviceWorker.register('sw.js').then((registration) => {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, (err) => {
