d11fbcd669fa632639987bc0af04ccbabf01b399
thulka2 <thulka2@uic.edu>
2021-03-27 20:11:47 -0500

    Updated SW again

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 let CACHE_NAME = 'my-site-cache-v1';
 let urlsToCache = [
   '/',
-  '/index.html'
+  
   
 ];
 
@@ -40,12 +40,12 @@ self.addEventListener('fetch', function(event) {
             // and because we want the browser to consume the response
             // as well as the cache consuming the response, we need
             // to clone it so we have two streams.
-            var responseToCache = response.clone();
+            // var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
+            // caches.open(CACHE_NAME)
+            //   .then(function(cache) {
+            //     cache.put(event.request, responseToCache);
+            //   });
 
             return response;
           }
